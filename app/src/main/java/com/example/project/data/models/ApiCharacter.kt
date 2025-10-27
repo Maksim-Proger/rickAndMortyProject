10ac8bc25bf83cc3104d1184fe44bbf1c18dd0b2
Maksim <i@mpozdnjakov.ru>
2025-10-27 23:43:30 +0300

    4 commit

--- app/src/main/java/com/example/project/data/models/ApiCharacter.kt
+++ app/src/main/java/com/example/project/data/models/ApiCharacter.kt
@@ -1,10 +1,28 @@
 package com.example.project.data.models
 
+import com.example.project.domain.models.Location
+import com.example.project.domain.models.Origin
+
 data class ApiCharacter(
     val id: Int,
     val name: String,
     val status: String,
     val species: String,
+    val type: String?,
     val gender: String,
-    val image: String
+    val origin: Origin?,
+    val location: Location?,
+    val image: String?,
+    val url: String?,
+    val created: String?
+)
+
+data class ApiOrigin(
+    val name: String,
+    val url: String
+)
+
+data class ApiLocation(
+    val name: String,
+    val url: String
 )
